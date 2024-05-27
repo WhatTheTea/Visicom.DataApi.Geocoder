c35f38710af8b0bc4522bf9404a5f6693e72b6d3
Oleksii Silchenko <silchenko.whatthetea@gmail.com>
2024-05-27 16:01:52 +0300

    Add exception tests

--- src/Visicom.DataApi.Geocoder.Tests/Unit/RequestTests.cs
+++ src/Visicom.DataApi.Geocoder.Tests/Unit/RequestTests.cs
@@ -10,17 +10,43 @@ namespace Visicom.DataApi.Geocoder.Tests.Unit;
 public class RequestTests : RequestTestsBase
 {
     private const string ApiKey = "API_KEY";
+    private const string RequestFormat = "https://api.visicom.ua/data-api/5.0/uk/geocode.json?key={0}&t={1}&order=relevance";
     private const string TestResponsePath = "TestData/IdealResponse.json";
-    private const string KyivRequest = $"https://api.visicom.ua/data-api/5.0/uk/geocode.json?key={ApiKey}&t=м. Київ, вул. Хрещатик, 26&order=relevance";
+    private const string ApiKeyInvalidResponsePath = "TestData/ApiKeyInvalidResponse.json";
+    private const string NotFoundResponsePath = "TestData/NotFoundResponse.json";
     public RequestTests()
     {
         var httpMock = new MockHttpMessageHandler();
-        var testResponse = File.ReadAllText(TestResponsePath);
-        httpMock.When("*")
-            .With(x => HttpUtility.UrlDecode(x.RequestUri?.ToString()) == KyivRequest)
-            .Respond("application/json", testResponse);
+        AddIdealMock();
+        AddNotFoundMock();
+        AddApiKeyInvalidMock();
         
         Geocoder = BuildGeocoder(httpMock.ToHttpClient());
+        return;
+
+        void AddNotFoundMock()
+        {
+            var notFoundResponse = File.ReadAllText(NotFoundResponsePath);
+            httpMock.When("*")
+                .With(x => HttpUtility.UrlDecode(x.RequestUri?.ToString()) == string.Format(RequestFormat, ApiKey, "ABCDEFGH"))
+                .Respond(MediaTypeNames.Application.Json ,notFoundResponse);
+        }
+
+        void AddApiKeyInvalidMock()
+        {
+            var apiKeyInvalidResponse = File.ReadAllText(ApiKeyInvalidResponsePath);
+            httpMock.When("*")
+                .With(x => HttpUtility.UrlDecode(x.RequestUri?.ToString()) == string.Format(RequestFormat, "ABCDEFGH", KyivSearchTerm))
+                .Respond(MediaTypeNames.Application.Json, apiKeyInvalidResponse);
+        }
+
+        void AddIdealMock()
+        {
+            var testResponse = File.ReadAllText(TestResponsePath);
+            httpMock.When("*")
+                .With(x => HttpUtility.UrlDecode(x.RequestUri?.ToString()) == string.Format(RequestFormat, ApiKey, KyivSearchTerm))
+                .Respond(MediaTypeNames.Application.Json, testResponse);
+        }
     }
 
     private static BasicGeocoder BuildGeocoder(HttpClient httpClient)
